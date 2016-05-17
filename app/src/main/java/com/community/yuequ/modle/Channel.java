514a6e80a3d828b5e0dc9fbe66df970b7ae72ff9
nanixiaoseng <nanizhoulin@sina.com>
2016-05-18 01:21:29 +0800

    增加解析类型

---
+++ app/src/main/java/com/community/yuequ/modle/Channel.java
@@ -0,0 +1,36 @@
+package com.community.yuequ.modle;
+
+/**
+ * Created by apple on 16/5/17.
+ */
+public class Channel {
+    /**
+     * {
+     "errorCode" : 200,
+     "errorMessage" : "查询成功",
+     "result" : [ {
+     "id" : 4,
+     "name" : "视频专题1",
+     "img_path" : "/subjectImg/20160516222811132.jpg",
+     "type" : "1"
+     }, {
+     "id" : 2,
+     "name" : "个梵蒂冈",
+     "img_path" : "/subjectImg/20160504021323125.jpg",
+     "type" : "1"
+     }, {
+     "id" : 3,
+     "name" : "儿童",
+     "img_path" : "/subjectImg/20160517090613928.jpg",
+     "type" : "2"
+     } ]
+     }
+     */
+    public int id;
+    public String name;
+    public String img_path;
+    public String type;
+
+
+
+}
